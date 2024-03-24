19dba104c1678920332ba010915db5b47efbe4a6
Đặng Văn toàn <77764003+vantoanpaya@users.noreply.github.com>
2024-03-24 21:34:49 +0700

    Update starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-f739c773-630d-4c33-985d-47264be9416c/"
 
 OUTPUT_DIR="logs"
 
